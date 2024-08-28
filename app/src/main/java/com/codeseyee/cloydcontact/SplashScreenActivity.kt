c2d9d1cd0484bb2579e662a20d4358c22e332180
AAK <fyp786786786@gmail.com>
2024-08-28 09:55:05 -0700

    Login signup done

---
+++ app/src/main/java/com/codeseyee/cloydcontact/SplashScreenActivity.kt
@@ -0,0 +1,31 @@
+package com.codeseyee.cloydcontact
+
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import com.codeseyee.cloydcontact.ViewModel.SplashScreenViewModel
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    private val splashScreenViewModel: SplashScreenViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        splashScreenViewModel.navigateToLogin.observe(this, Observer { shouldNavigate ->
+            if (shouldNavigate) {
+                navigateToLogin()
+            }
+        })
+    }
+
+    private fun navigateToLogin() {
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+}
---
+++ app/src/main/java/com/codeseyee/cloydcontact/ViewModel/SplashScreenViewModel.kt
@@ -0,0 +1,29 @@
+package com.codeseyee.cloydcontact.ViewModel
+
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.*
+
+class SplashScreenViewModel : ViewModel() {
+
+    private val _navigateToLogin = MutableLiveData<Boolean>()
+    val navigateToLogin: LiveData<Boolean> get() = _navigateToLogin
+
+    init {
+        startSplashScreenTimer()
+    }
+
+    private fun startSplashScreenTimer() {
+        CoroutineScope(Dispatchers.Main).launch {
+            delay(1500)
+            _navigateToLogin.value = true
+        }
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        CoroutineScope(Dispatchers.Main).cancel()
+    }
+}
